3cd5c231a051fe0e2d2e505a3149392e9fa44673
Eric Ramirez <ramirez1990rer@gmail.com>
2023-03-25 18:03:15 +0200

    added npm init

---
+++ contacts.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.resolve('db/contacts.js');
+
+console.log(contactsPath);
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
